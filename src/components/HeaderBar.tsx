718551ccbdfb4fa568ed08686322b4dccace12ed
Walefy D. Gonçalves <walefyd1@gmail.com>
2023-06-10 16:18:56 -0300

    feat/botão de carrinho adicionado

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,5 +1,6 @@
 import { styled } from 'styled-components';
 import { InputWithSearchIcon } from './Input';
+import { ShoppingBagIcon } from './IconsComponets';
 
 const HeaderContainer = styled.header`
   display: flex;
@@ -18,11 +19,59 @@ const Logo = styled.a`
   text-decoration: none;
 `
 
+const InputAndBagContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 24px;
+`;
+
+const BagButton = styled.button`
+  line-height: 0;
+  border: none;
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
+
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DotCount = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  background-color: var(--delete-color);
+  top: 80%;
+  right: -30%;
+  padding: 5px;
+  transform: translateY(-50%);
+  width: 20px;
+  height: 20px;
+  border-radius: 100%;
+
+  font-family: Saira;
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 26px;
+  letter-spacing: 0em;
+  color: white;
+`;
 export function HeaderBar() {
+  const countProducts = ['', ''];
   return (
     <HeaderContainer>
       <Logo>capputeeno</Logo>
-      <InputWithSearchIcon placeholder="Procurando por algo expecífico?" />
+      <InputAndBagContainer>
+        <InputWithSearchIcon placeholder="Procurando por algo expecífico?" />
+        <BagButton>
+          <ShoppingBagIcon />
+          {countProducts.length > 0 && <DotCount>{countProducts.length}</DotCount>}
+        </BagButton>
+      </InputAndBagContainer>
     </HeaderContainer>
   );
 }
